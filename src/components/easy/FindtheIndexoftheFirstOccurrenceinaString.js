366c616e30be7cd28a8198a8ff53f9f78ceb4014
kaju111 <saikiakaju275@gmail.com>
2024-02-25 20:47:11 +0530

    solved

--- src/components/easy/FindtheIndexoftheFirstOccurrenceinaString.js
+++ src/components/easy/FindtheIndexoftheFirstOccurrenceinaString.js
@@ -1,6 +1,23 @@
 import React from "react";
 
 const FindtheIndexoftheFirstOccurrenceinaString = () => {
+  function Find(haystack, needle) {
+    k = 0;
+    for (let i = 0; i < haystack.length; i++) {
+      if (haystack[i] == needle[k]) {
+        k++;
+        if (k == needle.length) {
+          return i - (k - 1);
+        }
+      } else {
+        i = i - k;
+        k = 0;
+      }
+    }
+
+    return -1;
+  }
+
   return (
     <div>
       <h3>
